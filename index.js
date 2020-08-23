3995b106b2ed500b808ae3ee6ba50753722f6a9e
GGSargsyan <grigor.sargsyan@yahoo.com>
2020-08-23 14:28:29 -0700

    Added routing, JSON parsing, and POST with fetch

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const express = require('express');
 const app = express();
 app.listen(3003, () => console.log('listening at post 3003'));
 app.use(express.static(__dirname + '/'));
+
+//Gives server the ability to parse incoming data as json
+app.use(express.json({limit: '1mb'}));
+
+app.post('/api', (request, response) => {
+    //Request
+    console.log(request.body);
+    //Response
+    const data = request.body;
+    response.json({
+        status: 'success',
+        lattitude: data.lat,
+        longitude: data.lon
+    });
+});
