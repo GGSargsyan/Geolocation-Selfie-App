2a47518ad44c06c9de42804814bb5e902b76e7f9
GGSargsyan <grigor.sargsyan@yahoo.com>
2020-08-29 20:21:03 -0700

    Added a database using NeDB

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 app.listen(3003, () => console.log('listening at post 3003'));
 app.use(express.static(__dirname + '/'));
@@ -6,7 +7,10 @@ app.use(express.static(__dirname + '/'));
 //Gives server the ability to parse incoming data as json
 app.use(express.json({limit: '1mb'}));
 
-const database = [];
+const database = new Datastore('database.db');
+//Gets the database file or creates new db if not in directory
+database.loadDatabase();
+database.insert({name: 'Grigor', status: 'alive'});
 
 app.post('/api', (request, response) => {
     //Request
