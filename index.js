5c13c02b02fef483d56820a20d335201869da190
GGSargsyan <grigor.sargsyan@yahoo.com>
2020-08-29 20:05:48 -0700

    Changed submit button to save data to array

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ app.use(express.static(__dirname + '/'));
 //Gives server the ability to parse incoming data as json
 app.use(express.json({limit: '1mb'}));
 
+const database = [];
+
 app.post('/api', (request, response) => {
     //Request
-    console.log(request.body);
+    console.log('request received');
+    //console.log(request.body);
     //Response
     const data = request.body;
+    database.push(data);
+    console.log(database);
     response.json({
         status: 'success',
         lattitude: data.lat,
