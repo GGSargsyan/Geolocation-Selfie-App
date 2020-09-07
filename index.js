24e7b7941aeb18c2ba73154280e2685d14b39c12
GGSargsyan <grigor.sargsyan@yahoo.com>
2020-09-06 20:48:03 -0700

    Added new html page home.html and added new api 'get' routing

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ app.use(express.json({limit: '1mb'}));
 const database = new Datastore('database.db');
 //Gets the database file or creates new db if not in directory
 database.loadDatabase();
-database.insert({name: 'Grigor', status: 'alive'});
+
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if(err){
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
+});
+
 
 app.post('/api', (request, response) => {
     //Request
@@ -18,11 +28,15 @@ app.post('/api', (request, response) => {
     //console.log(request.body);
     //Response
     const data = request.body;
-    database.push(data);
-    console.log(database);
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    
     response.json({
         status: 'success',
+        timestamp: timestamp,
         lattitude: data.lat,
-        longitude: data.lon
+        longitude: data.lon,
+        text: data.userSaid
     });
 });
